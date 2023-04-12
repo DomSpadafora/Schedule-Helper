139ce6b05bf11cd27f290a0c3e9b426497bcccff
DomSpadafora <120040191+DomSpadafora@users.noreply.github.com>
2023-04-12 12:08:12 -0400

    hours

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -7,50 +7,32 @@ function displayTime() {
 displayTime()
 setInterval(displayTime, 1000);
 
+if(localStorage.hours) {
+  let store = JSON.parse(localStorage.hours);
 
-function readProjectsFromStorage() {
-  var toDo = localStorage.getItem('toDo');
-  if (toDo) {
-    toDo = JSON.parse(toDo);
-  } else {
-    projects = [];
+  for (let i = 0; i < store.length; i++) {
+    $('textarea').eq(i).val(store[i])
   }
-  return projects;
+}
+
+$('.saveBtn').on('click', function () {
+  console.log('you clicked me');
+
+  let store = [];
+
+  for (let i = 0; i < $('textarea').length; i++) {
+    store.push($('textarea').eq(i).val());
+  };
+
+  localStorage.hours = JSON.stringify(store);
+});
+
+if (projectDate.isBefore(today)) {
+  rowEl.addClass('project-late');
+} else if (projectDate.isSame(today)) {
+  rowEl.addClass('project-today');
 }
 
 
-$(function  () {
-    // TODO: Add a listener for click events on the save button. This code should
-
-    $('.saveBtn').on('click', function () {
-      console.log('you clicked me');
-
-      let store = []
-    });
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
 
 
-    function saveProjectsToStorage(projects) {
-      localStorage.setItem('projects', JSON.stringify(projects));
-    }
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    
-    
-
-
-  });
-  
